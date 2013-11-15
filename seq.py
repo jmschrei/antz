7b89289e6b551e3c50883aa2dca85f95ca6c153f
Jacob Schreiber <jacobtribe@yahoo.com>
2013-11-14 23:59:35 -0800

    Added hydropathy

--- seq.py
+++ seq.py
@@ -104,7 +104,7 @@ class Sequence( object ):
 		alphabet = alphabet or self.__class__.alphabet
 		return composition( self.sequence, alphabet=alphabet, window=window )
 
-	def plot( self, x, window=None, title=None, c='c', alpha=0.8, linewidth=2, **kwargs ):
+	def plot( self, x, window=None, title=None, c='c', alpha=0.8, linewidth=1.5, **kwargs ):
 		x, xlabel = getattr( self, x )( window=window ), x
 
 		if type(x) == dict:
@@ -277,3 +277,14 @@ class Protein( Sequence ):
 	_pKa = { 'D': 3.9, 'E': 4.3, 'R': 12.0, 'K': 10.5, 'H': 6.08, 'C': 8.28, 
 			'Y': 10.1 }
 
+	# Hydropathy scores found at http://gcat.davidson.edu/DGPB/kd/aminoacidscores.htm
+	_hydropathy = { 'I': 4.5, 'V': 4.2, 'L': 3.8, 'F': 2.8, 'C': 2.5, 'M': 1.9,
+					'A': 1.8, 'G': -0.4, 'T': -0.7, 'W': -0.9, 'S': -0.8,
+					'Y': -1.3, 'P': -1.6, 'H': -3.2, 'E': -3.5, 'Q': -3.5,
+					'D': -3.5, 'N': -3.5, 'K': -3.9, 'R': -4.5 }
+
+	def hydropathy( self, window=None ):
+		add = lambda x, y: x + self.__class__._hydropathy[y]
+		if not window:
+			window = self.n
+		return [ reduce( add, self.sequence[i:i+window], 0 ) for i in xrange( self.n-window+1 ) ]
